cc1eaf1d81400037e1dae2a2c0ab09b09be5f956
LAI-1048576 <7530270+index-1048576@user.noreply.gitee.com>
2020-05-24 12:23:19 +0800

    update golang/simple.go.

--- golang/simple.go
+++ golang/simple.go
@@ -9,3 +9,6 @@ func pow(base float64,exp float64)(float64){
 func root(base float64,exp float64)(float64){
     return pow(base,1/exp)
 }
+func sqrt(num float64)(float64){
+    return root(num,2.0)
+}
