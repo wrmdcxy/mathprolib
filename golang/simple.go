06a93de8f98899f85234a64019f532d36629b0e3
2097152 <cookies_wrmdcxy@126.com>
2020-05-24 13:38:41 +0800

    debug*1()

--- golang/simple.go
+++ golang/simple.go
@@ -3,12 +3,12 @@ import (
     _ "fmt"
     "math"
 )
-func pow(base float64,exp float64)(float64){
+func Pow(base float64,exp float64)(float64){
     return math.Pow(base,exp) 
 }
-func root(base float64,exp float64)(float64){
+func Root(base float64,exp float64)(float64){
     return pow(base,1/exp)
 }
-func sqrt(num float64)(float64){
+func Sqrt(num float64)(float64){
     return root(num,2.0)
 }
