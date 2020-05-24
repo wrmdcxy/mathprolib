5647146370eed76ec350e72f36704256f3077266
2097152 <cookies_wrmdcxy@126.com>
2020-05-24 11:42:56 +0800

    rename go.go to golang/simple.go.

---
+++ golang/simple.go
@@ -0,0 +1,11 @@
+package mathprolib
+import (
+    "fmt"
+    "math"
+)
+func pow(base float64,exp float64)(float64){
+    return math.pow(base,exp) 
+}
+func root(base float64,exp float64)(float64){
+    return pow(base,1/exp)
+}
