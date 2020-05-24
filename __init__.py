1127898d296364a59a20ab4c8bcdb9fcdd5a6ca2
LAI-1048576 <7530270+index-1048576@user.noreply.gitee.com>
2020-05-24 09:01:56 +0800

    update __init__.py.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,26 @@
 # Version: Test
 # Author:LAI-1048576,Toby-LAI
 # -*- coding:utf-8 -*-
+
+import math
+from functools import total_ordering
+
+
+e = math.e
+π = math.pi
+Ф = (math.sqrt(5)-1)/2
+γ = 0.5772156649015328
+@total_ordering
+class INF:
+    def __lt__(self, other):
+        return False
+
+    def __eq__(self, other):
+        return isinstance(other,INF)
+
+    def __str__(self):
+        return "∞"
+
+Infinity = INF()
+
+
