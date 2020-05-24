5f1e7bc96c358471a4f763de61c95b8a4ed0407c
2097152 <cookies_wrmdcxy@126.com>
2020-05-24 14:24:49 +0800

    增加Round、Floor、Ceil并测试通过

--- golang/simple.go
+++ golang/simple.go
@@ -1,14 +1,23 @@
 package mathprolib
 import (
-    _ "fmt"
-    "math"
+	_ "fmt"
+	"math"
 )
 func Pow(base float64,exp float64)(float64){
-    return math.Pow(base,exp) 
+	return math.Pow(base,exp) 
 }
 func Root(base float64,exp float64)(float64){
-    return Pow(base,1/exp)
+	return Pow(base,1/exp)
 }
 func Sqrt(num float64)(float64){
-    return Root(num,2.0)
+	return Root(num,2.0)
+}
+func Round(num float64)(float64){
+	return math.Floor(num+0.5)
+}
+func Floor(num float64)(float64){
+	return math.Floor(num)
+}
+func Ceil(num float64)(float64){
+	return math.Ceil(num)
 }
