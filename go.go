a5204d81e4ad87e56a5a1352dc4e107078bf48ac
LAI-1048576 <7530270+index-1048576@user.noreply.gitee.com>
2020-05-24 11:04:11 +0800

    update go.go.

--- go.go
+++ go.go
@@ -1,3 +1,10 @@
 package main
-import "fmt"
-func main{}
+import ("fmt"
+        "math"
+)
+func pow(base float64,exp float64 )(float64){
+    return math.pow(base,exp) 
+}
+func root(base float64,exp float64)(float64){
+        return pow(base,1/exp)
+}
