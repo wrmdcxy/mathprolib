c203d65ea1bff78a04d14ac1c1d1cde76a11d1ed
2097152 <cookies_wrmdcxy@126.com>
2020-05-24 15:40:15 +0800

    测试文件

--- golang/simple_test.go
+++ golang/simple_test.go
@@ -7,7 +7,6 @@ import "testing"
 //Tested.ok
 //1:Fail,3.326s
 //2:ok,3.369s
-//3:ok,3.332s
 func TestPowandSqrtandRoot(t *testing.T){
 	if pr:=Pow(2.0,3.0);pr!=8.0{
 		t.Fatal("Failed,result=",pr,".The result should be 4.")
@@ -29,7 +28,6 @@ func TestPowandSqrtandRoot(t *testing.T){
 
 //Tested.ok
 //2:ok,3.369s
-//3:ok,3.332s
 func TestRound(t *testing.T){
 	if res:=Round(0.56772981827312897716);res!=1.0{
 		t.Fatal("Failed,result=",res,".The result should be 1.")
@@ -42,12 +40,17 @@ func TestRound(t *testing.T){
 //Tested.ok
 //3:ok,3.332s
 func TestFloorandCeil(t *testing.T){
-	if res,res2:=Floor(2.361237633772981827312897716),Ceil(1.129376215362789127888827763663287);res!=2|| res2!=2{
+	if res,res2:=Floor(2.361237633772981827312897716),Ceil(1.129376215362789127888827763663287);res!=2.0 || res2!=2.0{
 		t.Fatal("Failed,result=",res,res2,".The result should be 2,2.")
 	}else{
 		t.Log("Success.",res,res2)
 	}
 }
+
+
+//Tested.ok
+//4:Failed [build failed]
+//5:ok,3.407s
 func TestAbs(t *testing.T){
 	if res,res2:=Abs(-2.5),Abs(5.6);res != 2.5||res2 != 5.6{
 		t.Fatal("Failed,result=",res,res2,".The result should be 2.5,5.6.")
