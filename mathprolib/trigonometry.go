5c949fe1c3ff98dbdca562c21434b6acd50d41a8
LAI-1048576 <7530270+index-1048576@user.noreply.gitee.com>
2020-05-27 08:16:38 +0800

    update mathprolib/trigonometry.go.

--- mathprolib/trigonometry.go
+++ mathprolib/trigonometry.go
@@ -1,5 +1,11 @@
 package mathprolib
 import "math"
+func Deg(num float64)(float64){
+    return num*180/math.Pi
+}
+func Rad(num float64)(float64){
+    return num/180*math.Pi
+}
 
 Sinh:=m.Sin
 Cosh:=m.Cos
@@ -7,4 +13,7 @@ Tanh:=m.Tan
 Asinh:=m.Asin
 Acosh:=m.Acos
 Atanh:=m.Atan
-Sincos:=m.Sincos
+Sincosh:=m.Sincos
+Sin:=func(float64 num)(float64){return Sinh(Rad(num))}
+Cos:=func(float64 num)(float64){return Cosh(Rad(num))}
+Tan:=func(float64 num)(float64){return Tanh(Rad(num))}
