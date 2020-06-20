8f6591d3f4362e9f3fbfc01938dcd4ff29c432ea
LAI-1048576 <7530270+index-1048576@user.noreply.gitee.com>
2020-06-20 11:37:24 +0800

    rename mathprolib/float2VulgarFraction.py to mathprolib/float2Fraction.py.

--- mathprolib/float2Fraction.py
+++ mathprolib/float2Fraction.py
@@ -5,4 +5,7 @@ def float2VulgarFraction(num:float):
         i+=1
         if abs(round(num*i)-num*i) < 1e-3:
             return VulgarFraction(round(num*i),i)
+
+def float2MixedFraction(num:float):
+    return float2VulgarFraction(num).toMixed()
         
