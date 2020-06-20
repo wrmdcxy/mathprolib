83e3ee47512d55716fa6f98d9bb985cd821749bf
LAI-1048576 <7530270+index-1048576@user.noreply.gitee.com>
2020-06-20 11:25:23 +0800

    add mathprolib/float2VulgarFraction.py.

---
+++ mathprolib/float2VulgarFraction.py
@@ -0,0 +1,8 @@
+from Fraction import VulgarFraction
+def float2VulgarFraction(num:float):
+    i = 1
+    while True:
+        i+=1
+        if abs(round(num*i)-num*i) < 1e-3:
+            return VulgarFraction(round(num*i),i)
+        
